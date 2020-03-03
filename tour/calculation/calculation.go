0313c815c9d77a5e0cb142884fe63ccc398277fe
shishuwu <shuwu.shi@gmail.com>
2020-03-03 16:55:20 +0800

    adjust code structure

--- tour/calculation/calculation.go
+++
@@ -1,6 +0,0 @@
-package calculation
-
-func Do_add(num1 int, num2 int) (int) {
-	sum := num1 + num2
-	return sum
-}
--- tour/package/package_sample.go
+++ tour/package/package_sample.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github/shishuwu/my-go-mod-sample/tour/calculation"
+	"github/shishuwu/my-go-mod-sample/common/calc"
 )
 
 func main() {
 	x, y := 15, 10
-	sum := calculation.Do_add(x, y)
+	sum := calc.Do_add(x, y)
 
 	fmt.Println("sum", sum)
 }
